578dfc24bf8bed500fac5278b2faaec829f195c6
_Meryam_ <itsmeryamkhan@gmail.com>
2026-08-09 20:11:39 -0400

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,14 @@
+integer_type = 17
+float_type = 3.14
+bool_type = True
+string_type = "Apple"
+list_type = [3,4,5]
+
+
+
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
