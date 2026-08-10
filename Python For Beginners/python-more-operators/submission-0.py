6776953ec80bee699cce8aa100844117c9f4f060
_Meryam_ <itsmeryamkhan@gmail.com>
2026-08-10 17:20:51 -0400

    Add: python-more-operators - submission-0

---
+++ Python For Beginners/python-more-operators/submission-0.py
@@ -0,0 +1,13 @@
+a, b, c = 2, 8, 5
+
+floor = ((a * b) // c)
+print(floor)
+
+remainder = ((a * b) % c)
+print(remainder)
+
+expo_one = (a ** b)
+print(expo_one)
+
+expo_two = (b ** c)
+print(expo_two)
