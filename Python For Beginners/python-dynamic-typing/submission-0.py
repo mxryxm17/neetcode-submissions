7d5c426a5ca411220e0dc2429376631cc484d073
_Meryam_ <itsmeryamkhan@gmail.com>
2026-08-09 20:22:36 -0400

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable = 2.5
+print(type(variable))
+variable = False
+print(type(variable))
+variable = "Laptop"
+print(type(variable))
+variable = [3, 6, 9]
+print(type(variable))
